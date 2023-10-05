f6ea6f91726bf3e64b6f91e1e890fbe9345472bb
unknown <mateusbertolini@yahoo.com.br>
2023-10-05 13:11:49 -0300

    ajustes back-end requisitar material e correções menores de código

--- scripts/alertMessage.js
+++ scripts/alertMessage.js
@@ -20,6 +20,27 @@
     };
   }
 
+  function showCustomInform(message) {
+    var customAlert = document.getElementById("customAlert");
+    var alertMessage = document.getElementById("alertMessage");
+    var okButton = document.getElementById("okButton");
+
+    alertMessage.textContent = message;
+    customAlert.style.display = "block";
+
+    var closeElements = document.getElementsByClassName("close");
+
+    for (var i = 0; i < closeElements.length; i++) {
+        closeElements[i].onclick = function () {
+            customAlert.style.display = "none";
+        };
+    }
+
+    okButton.onclick = function () {
+        customAlert.style.display = "none";
+    };
+  }
+
   function showCustomConfirm(message, callback) {
     var customConfirm = document.getElementById("customConfirm");
     var confirmMessage = document.getElementById("confirmMessage");
